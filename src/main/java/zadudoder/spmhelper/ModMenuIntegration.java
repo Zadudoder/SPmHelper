b4842697528a7953c42a04c6f2d14f80c4000a65
R4mBLe_ <misharad2008@gmail.com>
2025-05-21 22:42:03 +0300

    fix: crash on settings

--- src/main/java/zadudoder/spmhelper/ModMenuIntegration.java
+++ src/main/java/zadudoder/spmhelper/ModMenuIntegration.java
@@ -3,13 +3,14 @@ package zadudoder.spmhelper;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.NoticeScreen;
 import net.minecraft.text.Text;
-import zadudoder.spmhelper.Screen.Settings;
+import zadudoder.spmhelper.config.SPmHelperConfig;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
@@ -18,7 +19,7 @@ public class ModMenuIntegration implements ModMenuApi {
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> {
             if (FabricLoader.getInstance().isModLoaded("cloth-config")) {
-                return new Settings();
+                return AutoConfig.getConfigScreen(SPmHelperConfig.class, parent).get();
             }
             return new NoticeScreen(() -> MinecraftClient.getInstance().setScreen(parent), Text.of("SPmHelper"), Text.of("SPmHelper requires Cloth Config to configure the mod"));
         };
